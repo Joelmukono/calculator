6d20a41ba8daa80f7da54d746e813325844bd8de
joel <jlmukono@gamil.com>
2019-02-12 07:33:48 +0300

    add html,css and javascript files.Link javascript to html

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+var add = function(number1,number2){
+  return number1 + number2;
+};
+var number1=parseInt(prompt('enter number'));
+var number2=parseInt(prompt('enter number'));
+alert(add(number1,number2));
+
+var subtract = function(number1, number2){
+  return number1 - number2;
+};
+alert(subtract(5,4));
